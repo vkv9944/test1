dce45b09f7645e24a267fe6f64cb22ebc91ee1af
vivek kumar <vivek199575@gmail.com>
2021-03-07 21:25:24 +0530

    'first'

---
+++ app.js
@@ -0,0 +1,63 @@
+const express=require("express");
+const mysql=require("mysql");
+const dotenv=require('dotenv');
+const path=require('path');
+
+dotenv.config({ path: './.env'});   
+
+const app=express();
+
+const db=mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+const publicDirectory= path.join(__dirname,'./public');
+//console.log(__dirname);
+
+
+//Parse URL-encoded bodies (as sent by html forms)
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+app.use(express.static(publicDirectory));
+
+app.set('view engine','hbs');
+
+db.connect( (error) =>{
+ if(error){
+     console.log(error);
+
+ }
+ else{
+     console.log("MySql connected");
+ }
+
+})
+
+/*
+//DEFine rotes previous
+app.get("/",(req,res)=>{
+    //res.send("<h1>Home page</h1>")
+    res.render("index")
+});
+
+
+app.get("/register",(req,res)=>{
+    //res.send("<h1>Home page</h1>")
+    res.render("register")
+}); */
+
+
+//Define routes
+
+app.use('/',require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
+
+
+app.listen(5002,()=>{
+    console.log("server started on 5002");
+})
---
+++ controllers/auth.js
@@ -0,0 +1,65 @@
+const mysql=require('mysql');
+
+const jwt =require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
+
+const db=mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+const { request } = require("express");
+
+exports.register=(req,res)=>{
+
+    console.log(req.body);
+
+//const name=request.body.name;
+//const email=request.body.email;
+//const password=request.body.password;
+//const passwordConfirm=req.body.passwordConfirm;
+
+//we can use above code in simple as bellow
+
+const {name, email,password,passwordConfirm}=req.body;
+
+
+db.query('SELECT email from users WHERE email = ?',[email],async (error,results)=>{
+    if(error){
+        console.log(error);
+    }
+    if(results.length > 0) {
+        return res.render('register',{
+            message:'this email already exist'
+           });
+    }  else if( password != passwordConfirm){
+        return res.render('register',{
+            message:'password not matched'
+           });
+    }
+
+   
+
+  let hashedPassword= await bcrypt.hash(password, 8);
+  console.log(hashedPassword);
+  
+  db.query('INSERT INTO users SET ?',{name: name,email:email, password:hashedPassword},(error, results)=>{
+
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        return res.render('register',{
+         message:'user registered'
+        });
+    }
+
+  })
+
+})
+
+
+  //  res.send("form submitted");
+}
